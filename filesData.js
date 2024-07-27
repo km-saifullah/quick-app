35ee29ce6d7a076265f0ee1e930d2d2ee56888fd
km-saifullah <kmsaifullah16@gmail.com>
2024-07-27 20:48:31 +0600

    import files code from data module

---
+++ filesData.js
@@ -0,0 +1,140 @@
+// server.js data
+const serverCode = `const app = require("./app.js")
+const connectDb = require("./db/connectDb")
+const serverPort = require("./config/index")
+
+// connect database
+connectDb()
+
+app.listen(serverPort, () => console.log("Server is running"))`;
+
+// app.js data
+const appCode = `const express = require("express")
+
+const app = express();
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+module.exports = app`;
+
+// config data
+const configCode = `const { configDotenv } = require("dotenv")
+
+configDotenv()
+
+const serverPort = process.env.PORT
+const dbUrl = process.env.DATABASE_URL
+
+module.exports = { serverPort, dbUrl }`;
+
+// connectDB data
+const dbConnectionCode = `const mongoose = require("mongoose")
+const dbUrl = require("./../config/index")
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(dbUrl)
+    console.log("Database Connected")
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+module.exports = connectDb`;
+
+// prettierrc data
+const prettierCode = `{
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "bracketSpacing": true
+}`;
+
+// prettierignore data
+const prettyIgnoreCode = `/.vscode
+/node modules
+•/dist
+*.env
+.env
+.env.*`;
+
+// gitignore data
+const gitIgnoreCode = `# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+lerna-debug.log*
+.pnpm-debug.log*
+
+# Bower dependency directory (https://bower.io/)
+bower_components
+
+# node-waf configuration
+.lock-wscript
+
+# Compiled binary addons (https://nodejs.org/api/addons.html)
+build/Release
+
+# Dependency directories
+node_modules/
+jspm_packages/
+
+# Snowpack dependency directory (https://snowpack.dev/)
+web_modules/
+
+# TypeScript cache
+*.tsbuildinfo
+
+# Optional npm cache directory
+.npm
+
+# Optional eslint cache
+.eslintcache
+
+# Optional REPL history
+.node_repl_history
+
+# Output of 'npm pack'
+*.tgz
+
+# Yarn Integrity file
+.yarn-integrity
+
+# dotenv environment variable files
+.env
+.env.development.local
+.env.test.local
+.env.production.local
+.env.local
+
+# vuepress v2.x temp and cache directory
+.temp
+.cache
+
+# Serverless directories
+.serverless/
+
+# Stores VSCode versions used for testing VSCode extensions
+.vscode-test
+
+# yarn v2
+.yarn/cache
+.yarn/unplugged
+.yarn/build-state.yml
+.yarn/install-state.gz
+.pnp.*`;
+
+module.exports = {
+  serverCode,
+  appCode,
+  configCode,
+  dbConnectionCode,
+  prettierCode,
+  prettyIgnoreCode,
+  gitIgnoreCode,
+};
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const createDirectory = require("./createDirectory");
 const createFile = require("./createFile");
+const {
+  serverCode,
+  appCode,
+  configCode,
+  dbConnectionCode,
+  prettierCode,
+  prettyIgnoreCode,
+  gitIgnoreCode,
+} = require("./filesData");
 
 // create .env file
 createFile(
@@ -19,150 +28,13 @@ createDirectory("./src/config");
 createDirectory("./src/utils");
 
 // make files
-createFile(
-  "./src/server.js",
-  `const app = require("./app.js")
-const connectDb = require("./db/connectDb")
-const serverPort = require("./config/index")
-
-// connect database
-connectDb()
-
-app.listen(serverPort, () => console.log("Server is running"))`
-);
-
-createFile(
-  "./src/app.js",
-  `const express = require("express")
-
-const app = express()
-
-// middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-module.exports = app`
-);
-
-createFile(
-  "./src/config/index.js",
-  `const { configDotenv } = require("dotenv")
-
-configDotenv()
-
-const serverPort = process.env.PORT
-const dbUrl = process.env.DATABASE_URL
-
-module.exports = { serverPort, dbUrl }`
-);
-
-createFile(
-  "./src/db/connectDb.js",
-  `const mongoose = require("mongoose")
-const dbUrl = require("./../config/index")
-
-const connectDb = async () => {
-  try {
-    await mongoose.connect(dbUrl)
-    console.log("Database Connected")
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-module.exports = connectDb`
-);
-
-createFile(
-  "./.prettierrc",
-  `{
-  "tabWidth": 2,
-  "useTabs": false,
-  "semi": false,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "bracketSpacing": true
-}`
-);
-
-createFile(
-  "./.prettierignore",
-  `/.vscode
-/node modules
-•/dist
-*.env
-.env
-.env.*`
-);
-
-createFile(
-  "./.gitignore",
-  `# Logs
-logs
-*.log
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-lerna-debug.log*
-.pnpm-debug.log*
-
-# Bower dependency directory (https://bower.io/)
-bower_components
-
-# node-waf configuration
-.lock-wscript
-
-# Compiled binary addons (https://nodejs.org/api/addons.html)
-build/Release
-
-# Dependency directories
-node_modules/
-jspm_packages/
-
-# Snowpack dependency directory (https://snowpack.dev/)
-web_modules/
-
-# TypeScript cache
-*.tsbuildinfo
-
-# Optional npm cache directory
-.npm
-
-# Optional eslint cache
-.eslintcache
-
-# Optional REPL history
-.node_repl_history
-
-# Output of 'npm pack'
-*.tgz
-
-# Yarn Integrity file
-.yarn-integrity
-
-# dotenv environment variable files
-.env
-.env.development.local
-.env.test.local
-.env.production.local
-.env.local
-
-# vuepress v2.x temp and cache directory
-.temp
-.cache
-
-# Serverless directories
-.serverless/
-
-# Stores VSCode versions used for testing VSCode extensions
-.vscode-test
-
-# yarn v2
-.yarn/cache
-.yarn/unplugged
-.yarn/build-state.yml
-.yarn/install-state.gz
-.pnp.*`
-);
+createFile("./src/server.js", serverCode);
+createFile("./src/app.js", appCode);
+createFile("./src/config/index.js", configCode);
+createFile("./src/db/connectDb.js", dbConnectionCode);
+createFile("./.prettierrc", prettierCode);
+createFile("./.prettierignore", prettyIgnoreCode);
+createFile("./.gitignore", gitIgnoreCode);
 
 console.log(`Great Job 👏 Start Writing Code in your new Express App
 ⚠️ Before Writing Code | you need to install packages below:
