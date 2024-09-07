8d7505ee1e5eccab60a43c719d3ef535e7059043
km-saifullah <kmsaifullah16@gmail.com>
2024-09-07 10:44:43 +0600

    server export default problem fixed

--- server.js
+++ server.js
@@ -10,31 +10,33 @@ import {
   gitIgnoreCode,
 } from "./filesData.js";
 
-// create .env file
-createFile("./.env", `DATABASE_URL=your-mongodb-url`);
+const server = () => {
+  // create .env file
+  createFile("./.env", `DATABASE_URL=your-mongodb-url`);
 
-// make directories
-createDirectory("./src");
-createDirectory("./src/controllers");
-createDirectory("./src/models");
-createDirectory("./src/db");
-createDirectory("./src/routes");
-createDirectory("./src/middlewares");
-createDirectory("./src/config");
-createDirectory("./src/utils");
+  // make directories
+  createDirectory("./src");
+  createDirectory("./src/controllers");
+  createDirectory("./src/models");
+  createDirectory("./src/db");
+  createDirectory("./src/routes");
+  createDirectory("./src/middlewares");
+  createDirectory("./src/config");
+  createDirectory("./src/utils");
 
-// make files
-createFile("./src/server.js", serverCode);
-createFile("./src/app.js", appCode);
-createFile("./src/config/index.js", configCode);
-createFile("./src/db/connectDb.js", dbConnectionCode);
-createFile("./.prettierrc", prettierCode);
-createFile("./.prettierignore", prettyIgnoreCode);
-createFile("./.gitignore", gitIgnoreCode);
+  // make files
+  createFile("./src/server.js", serverCode);
+  createFile("./src/app.js", appCode);
+  createFile("./src/config/index.js", configCode);
+  createFile("./src/db/connectDb.js", dbConnectionCode);
+  createFile("./.prettierrc", prettierCode);
+  createFile("./.prettierignore", prettyIgnoreCode);
+  createFile("./.gitignore", gitIgnoreCode);
 
-console.log(`Great Job 👏 Start Writing Code in your new Express App
+  console.log(`Great Job 👏 Start Writing Code in your new Express App
 ⚠️ Before Writing Code | you need to install packages below:
 ##### install the packages below ####
 npm install express mongoose dotenv`);
+};
 
 export default server;
